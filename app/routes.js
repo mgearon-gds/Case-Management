4196fbb40e92c5278f6fb7ecab571c9ee4f9bf4f
mgearon-gds <103048725+mgearon-gds@users.noreply.github.com>
2023-11-14 14:51:59 +0000

    Changes

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,20 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.get('/', function (req, res) {
+    var fakeCases = require('./data/data.js')
+    req.session.cases = fakeCases
+    req.session.recents = []
+    req.session.notifications = {}
+    req.session.notifications.list = []
+
+    res.render('index', {
+        cases: req.session.cases
+      })
+  })
+// On post
+router.get('/case-manager', function (req, res) {
+    res.render('case-manager', {
+      cases: req.session.cases
+    })
+  })
