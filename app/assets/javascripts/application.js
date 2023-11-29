1900335ec908af9b03ac0eee888c8a6d6383a7cf
mgearon-gds <103048725+mgearon-gds@users.noreply.github.com>
2023-11-29 11:27:25 +0000

    Formatting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,33 +4,33 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-  const searchButton = document.getElementById('searchButton');
-  if (searchButton) {
-    searchButton.addEventListener('click', handleSearch);
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const searchButton = document.getElementById('searchButton');
+    if (searchButton) {
+      searchButton.addEventListener('click', handleSearch);
+    }
+  });
+
+  function handleSearch() {
+    const searchTerm = document.getElementById('search-cases').value.trim();
+    const currentUrl = window.location.href;
+
+    // Update the URL with the new search term
+    const updatedUrl = updateQueryStringParameter(currentUrl, 'search', searchTerm);
+
+    // Redirect to the updated URL
+    window.location.href = updatedUrl;
   }
-});
 
-function handleSearch() {
-  const searchTerm = document.getElementById('search-cases').value.trim();
-  const currentUrl = window.location.href;
+  function updateQueryStringParameter(uri, key, value) {
+    const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    const separator = uri.indexOf('?') !== -1 ? "&" : "?";
 
-  // Update the URL with the new search term
-  const updatedUrl = updateQueryStringParameter(currentUrl, 'search', searchTerm);
-
-  // Redirect to the updated URL
-  window.location.href = updatedUrl;
-}
-
-function updateQueryStringParameter(uri, key, value) {
-  const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-  const separator = uri.indexOf('?') !== -1 ? "&" : "?";
-
-  if (uri.match(re)) {
-    return uri.replace(re, '$1' + key + "=" + value + '$2');
-  } else {
-    return uri + separator + key + "=" + value;
+    if (uri.match(re)) {
+      return uri.replace(re, '$1' + key + "=" + value + '$2');
+    } else {
+      return uri + separator + key + "=" + value;
+    }
   }
-}
 })
