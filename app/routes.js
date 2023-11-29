ab7db80b5c03ab1e33af678b8e1adc6051418b8b
mgearon-gds <103048725+mgearon-gds@users.noreply.github.com>
2023-11-29 09:22:04 +0000

    Deleting old pages

--- app/routes.js
+++ app/routes.js
@@ -18,15 +18,10 @@ router.get('/', function (req, res) {
         cases: req.session.cases
       })
   })
-// On post
-router.get('/case-manager', function (req, res) {
-    res.render('case-manager', {
-      cases: req.session.cases
-    })
-  })
 
-  router.get('/forms-integration/case-manager', function (req, res) {
-    res.render('forms-integration/case-manager', {
+
+  router.get('/case-manager', function (req, res) {
+    res.render('case-manager', {
       cases: req.session.cases
     })
   })
